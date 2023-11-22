490d5ead4e1d56648cbe6c11baeb4bc34e7f5b0c
21020419PhamTuUyen <21020419@vnu.edu.vn>
2023-11-22 17:32:40 +0700

    terms&Condition and privacy policy

--- app/src/main/java/com/example/museum/account/profile/profile.java
+++ app/src/main/java/com/example/museum/account/profile/profile.java
@@ -12,6 +12,7 @@ import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import com.example.museum.Gioithieu.SplashSecond;
 import com.example.museum.Gioithieu.SplashThird;
@@ -19,6 +20,8 @@ import com.example.museum.MainActivity;
 import com.example.museum.R;
 import com.example.museum.Trangchu.Home;
 import com.example.museum.account.Account;
+import com.example.museum.account.TaP.TaP;
+import com.example.museum.account.createAccount.CreateAccount;
 
 import at.markushi.ui.CircleButton;
 import de.hdodenhof.circleimageview.CircleImageView;
@@ -27,29 +30,30 @@ public class profile extends  AppCompatActivity{
 
    Account account = new Account("test","abc") ;
 
+   ConstraintLayout editScreen;
+
    TextView username;
 
-   Button infoBtn;
    Button editBtn;
    Button policyBtn;
    Button logBtn;
+   Button closeBtn;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.user_profile);
         username = findViewById(R.id.username);
-        infoBtn = findViewById(R.id.infoBtn);
         editBtn = findViewById(R.id.editBtn);
         policyBtn = findViewById(R.id.policyBtn);
         logBtn = findViewById(R.id.logoutBtn);
+        closeBtn = findViewById(R.id.Close_Button);
 
         if(account == null){
             username.setText("xin vui lòng đăng nhập");
             Drawable icon = getResources().getDrawable(R.drawable.ic_log_in);
             editBtn.setCompoundDrawablesRelativeWithIntrinsicBounds(icon,null,null,null);
             editBtn.setText("Đăng nhập");
-            infoBtn.setVisibility(infoBtn.GONE);
             policyBtn.setVisibility(policyBtn.GONE);
             logBtn.setVisibility(logBtn.GONE);
 
@@ -64,9 +68,30 @@ public class profile extends  AppCompatActivity{
 
         }else{
             username.setText(account.getEmail());
-            infoBtn.setVisibility(infoBtn.VISIBLE);
             policyBtn.setVisibility(policyBtn.VISIBLE);
             logBtn.setVisibility(logBtn.VISIBLE);
+
+            editBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editScreen.setVisibility(View.VISIBLE);
+                }
+            });
+
+            closeBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    editScreen.setVisibility(View.GONE);
+                }
+            });
+
+            policyBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(profile.this, TaP.class).putExtra("from", "profile");
+                    startActivity(intent);
+                }
+            });
 
             logBtn.setOnClickListener(new View.OnClickListener() {
                 @Override
